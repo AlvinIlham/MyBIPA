8656cc82994edaf70f12eddfd9ecd461d372cf0c
AlvinIlham <alvinilham.pin@gmail.com>
2026-09-26 17:02:02 +0700

    chore: update synchronization URL endpoint

--- script.js
+++ script.js
@@ -3026,7 +3026,7 @@ const AKHIR = [
      penyimpanan di peramban masing-masing pengguna.
      ================================================================== */
   var VERSI_MODUL = "2026.09.16 \u00b7 jalur JSONP";
-  var URL_SINKRON = "https://script.google.com/macros/s/AKfycbzqjSvI1amxh-qJ5-1QcKehhUeLYyLerZ2VUz4TOW1Z5itoNICsGa-bnFmKbWwY3TfVaQ/exec";
+  var URL_SINKRON = "https://script.google.com/macros/s/AKfycbxWojIMsKD22uSzGRp65ub0vwLmSGH9M9uBYa_dPHMkSUO3s6xEewCcGKC0Bg9ISVVs/exec";
   var ID_BASISDATA = "1wURIPGZ7jZwNJhAD8Jq_VmRMunvwIjl_TJWacII9bm8";
 
   var GUDANG_OK = (function () {
